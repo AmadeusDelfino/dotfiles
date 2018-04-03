24f115bd1c023e496c2ee711c677f0c02d425f99
Amadeus Delfino <amadeus.delfino@convenia.com.br>
2018-04-03 20:37:30 -0300

    - Hide polybar script; Better docker alias and things

---
+++ i3/scripts/hide-polybar.sh
@@ -0,0 +1,18 @@
+xdo_path=$(which xdo)
+if [ $? -ne 0 ]; then
+    echo "Cannot find \`xdo\` command." 1>&2
+    exit 1
+fi
+# Obtain bar's window id
+bar_ids=$(xdo id -N "Polybar")
+bar_ids=$(echo $bar_ids | tr " " "\n")
+
+for bar_id in $bar_ids
+do
+    echo $bar_id
+    if xprop -id $bar_id | grep -q "Normal"; then
+        xdo hide $bar_id
+    else
+        $(sh  /home/death/.dotfiles/polybar/init.sh)
+    fi
+done
